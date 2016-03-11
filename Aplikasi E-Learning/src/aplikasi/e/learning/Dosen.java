ba6256fab0f44a5d074ba6f2cd47bd19910921b9
buddisantiko <buddisantiko@buddi>
2016-03-12 00:41:17 +0700

    belum ada kelas lainya

---
+++ Aplikasi E-Learning/src/aplikasi/e/learning/Dosen.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasi.e.learning;
+
+/**
+ *
+ * @author buddisantiko
+ */
+public class Dosen {
+    
+}
